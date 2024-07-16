ba5e5e77efab5bdc96cb29fb1636188f38440380
Subha M <subha7755@gmail.com>
2024-07-16 19:01:02 +0530

    Subha | Add two numbers as linked lists

--- src/sum-linked-lists/index.js
+++ src/sum-linked-lists/index.js
@@ -15,34 +15,21 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
- const add = (l1, l2, l3, carry = 0) => {
-    console.log('l1', l1)
-    console.log('l2', l2)
-    console.log('l3 entry', l3)
-    console.log('carry', carry)
-    console.log('----')
-
-    if(l1?.val && l2?.val){
+ const add = (l1, l2, carry = 0) => {
+    if(!l1 && !l2) {
+      return carry > 0 ? new ListNode(carry) : null
+    } else if(!l1 || !l2) {
+      const valueNode = l1 || l2
+      const val = valueNode.val + carry
+      const newNode = new ListNode(val % 10)
+      newNode.next = add(l1 ? l1.next : null, l2 ? l2.next : null, Math.trunc(val/10))
+      return newNode
+    } else {
         const val = l1.val + l2.val + carry
         const newNode = new ListNode(val%10)
-        const nextNode = add(l1.next, l2.next, newNode, Math.trunc(val/10))
-        if(!l3) return nextNode
-        else l3.next = nextNode
-    } else if(!l1 && !l2 && carry > 0){
-      const newNode = new ListNode(carry)
-      l3.next = newNode
-      return l3
-    } else if(!l1 && !l2 && carry === 0) {
-      console.log('$$$$$$$$l3 exit', l3)
-      return l3
-    } else if(!l2 && l1.val) {
-      console.log('$$$$$$$$ exit', l1)
-      return l1
+        newNode.next = add(l1.next, l2.next, Math.trunc(val/10))
+        return newNode
     }
-    console.log('l1 exit', l1)
-    console.log('l2 exit', l2)
-    console.log('**********l3 exit', l3)
-    return l3
  }
 var addTwoNumbers = function(l1, l2) {
     return add(l1, l2)
--- src/sum-linked-lists/index.spec.js
+++ src/sum-linked-lists/index.spec.js
@@ -1,10 +1,65 @@
 import addTwoNumbers, {ListNode} from './index'
 
 describe("addTwoNumbers", () => {
+    test('adding two null numbers with no carry', () => {
+      expect(addTwoNumbers(null, null)).toBeNull();
+    });
+
+    test('adding two zero numbers with no carry', () => {
+      const l1 = new ListNode(0)
+
+      expect(addTwoNumbers(l1, l1)).toEqual(l1);
+    });
+
+    test('adding null and number', () => {
+      const l1 = new ListNode(7)
+
+      const result = addTwoNumbers(null, l1)
+
+      expect(result.val).toEqual(7);
+      expect(result.next).toBeNull();
+    });
+
+    test('adding number and null', () => {
+      const l1 = new ListNode(7)
+
+      const result = addTwoNumbers(l1, null)
+
+      expect(result.val).toEqual(7);
+      expect(result.next).toBeNull();
+    });
+
+    test('adding number of length >1 and null', () => {
+      // 7 -> 2
+      const l1 = new ListNode(7)
+      const l2 = new ListNode(2)
+      l1.next = l2
+
+      const result = addTwoNumbers(l1, null)
+
+      expect(result.val).toEqual(7);
+      expect(result.next.val).toEqual(2);
+      expect(result.next.next).toBeNull();
+    });
+
+    test('adding null and number of length >1', () => {
+      // 7 -> 2
+      const l1 = new ListNode(7)
+      const l2 = new ListNode(2)
+      l1.next = l2
+
+      const result = addTwoNumbers(null, l1)
+
+      expect(result.val).toEqual(7);
+      expect(result.next.val).toEqual(2);
+      expect(result.next.next).toBeNull();
+    });
+
     test('adding two numbers of length 1 with no carry', () => {
       const l1 = new ListNode(7)
       const l2 = new ListNode(2)
       const l3 = new ListNode(9)
+
       expect(addTwoNumbers(l1, l2)).toEqual(l3);
     });
 
@@ -34,7 +89,7 @@ describe("addTwoNumbers", () => {
       n4.next=n5
 
       const l3 = addTwoNumbers(l1, l2)
-      console.log('result', l3)
+
       // l3 = 2 -> 7 -> 4
       expect(l3.val).toEqual(2);
       expect(l3.next.val).toEqual(7);
@@ -57,7 +112,7 @@ describe("addTwoNumbers", () => {
       n4.next=n5
 
       const l3 = addTwoNumbers(l1, l2)
-      console.log('result', l3)
+
       // l3 = 2 -> 7 -> 2 -> 1
       expect(l3.val).toEqual(2);
       expect(l3.next.val).toEqual(7);
@@ -66,24 +121,60 @@ describe("addTwoNumbers", () => {
       expect(l3.next.next.next.next).toBeNull();
     });
 
-    test('adding two numbers of differnet lengths', () => {
-      //l1 = 7 -> 2 -> 8
+    test('adding two numbers of different lengths with no carry', () => {
+      //l1 = 7 -> 3
       const l1 = new ListNode(7)
-      const n2 = new ListNode(2)
-      const n3 = new ListNode(8)
+      const n2 = new ListNode(3)
+      l1.next = n2
+
+      // l2 = 2
+      const l2 = new ListNode(2)
+
+      const l3 = addTwoNumbers(l1, l2)
+
+      // l3 = 9 -> 3
+      expect(l3.val).toEqual(9);
+      expect(l3.next.val).toEqual(3);
+      expect(l3.next.next).toBeNull();
+    });
+
+    test('adding two numbers of different lengths with  carry', () => {
+      //l1 = 7 -> 3
+      const l1 = new ListNode(7)
+      const n2 = new ListNode(3)
+      l1.next = n2
+
+      // l2 = 2
+      const l2 = new ListNode(4)
+
+      const l3 = addTwoNumbers(l1, l2)
+
+      // l3 = 1 -> 4
+      expect(l3.val).toEqual(1);
+      expect(l3.next.val).toEqual(4);
+      expect(l3.next.next).toBeNull();
+    });
+
+    test('adding two numbers of different lengths with  carry', () => {
+      //l1 = 9 -> 9 -> 9
+      const l1 = new ListNode(9)
+      const n2 = new ListNode(9)
+      const n3 = new ListNode(9)
       l1.next = n2
       n2.next = n3
-      // l2 = 5 -> 4
-      const l2 = new ListNode(5)
-      const n4 =  new ListNode(4)
+
+      // l2 = 9 -> 9
+      const l2 = new ListNode(9)
+      const n4 = new ListNode(9)
       l2.next = n4
 
       const l3 = addTwoNumbers(l1, l2)
-      console.log('result', l3)
-      // l3 = 2 -> 7 -> 8
-      expect(l3.val).toEqual(2);
-      expect(l3.next.val).toEqual(7);
-      expect(l3.next.next.val).toEqual(8);
-      expect(l3.next.next.next).toBeNull();
+
+      // l3 = 8 -> 9 -> 0 -> 1
+      expect(l3.val).toEqual(8);
+      expect(l3.next.val).toEqual(9);
+      expect(l3.next.next.val).toEqual(0);
+      expect(l3.next.next.next.val).toEqual(1);
+      expect(l3.next.next.next.next).toBeNull();
     });
 });
